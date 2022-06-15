4cc34de328a70f21c91534e50c6d3c0c41bcfe8f
MarceloFLopez <81024063+MarceloFLopez@users.noreply.github.com>
2022-06-14 22:45:41 -0300

    test: find/update

--- loja/src/main/java/br/com/loja/model/Categoria.java
+++ loja/src/main/java/br/com/loja/model/Categoria.java
@@ -1,5 +1,6 @@
 package br.com.loja.model;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
@@ -20,5 +21,6 @@ public class Categoria {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(length = 50)
 	private String nome;
 }
--- loja/src/main/java/br/com/loja/testConnection/CadastroDeProdutos.java
+++ loja/src/main/java/br/com/loja/testConnection/CadastroDeProdutos.java
@@ -1,33 +1,29 @@
 package br.com.loja.testConnection;
 
-import java.math.BigDecimal;
-
 import javax.persistence.EntityManager;
 
 import br.com.loja.model.Categoria;
-import br.com.loja.model.Produto;
-import br.com.loja.persistence.CategoriaDAO;
-import br.com.loja.persistence.ProdutoDAO;
 import br.com.loja.util.JPAUtil;
 
 public class CadastroDeProdutos {
 
 	public static void main(String[] args) {
 		Categoria categoria= new  Categoria(null, "CELULARES");
-		Produto celular = new Produto("Xaiomi Redme", "Muito Legal", new BigDecimal("800.00"), categoria);
-	
 
 		EntityManager em = JPAUtil.getEntityManager();
-		ProdutoDAO pdao = new ProdutoDAO(em);
-		CategoriaDAO cdao = new CategoriaDAO(em); 
 		
 		em.getTransaction().begin();
-		cdao.cadastrar(categoria);
-		pdao.cadastrar(celular);
-		
+		em.persist(categoria);
 		em.getTransaction().commit();
 		
-		Produto p = em.find(Produto.class,	 1l);
+		Categoria p = em.find(Categoria.class,	 1l);
+		
+		System.out.println(p);
+		
+		em.getTransaction().begin();
+		em.persist(p);
+		p.setNome("XPTO");
+		em.getTransaction().commit();
 		System.out.println(p);
 		em.close();
 
